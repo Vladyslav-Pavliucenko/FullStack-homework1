46f73f0dfddcf69e8216af6bd3c4ee86598fe34e
Vladyslav-Pavliucenko <topalevlad56@gmail.com>
2026-03-22 19:09:15 +0200

    add work

---
+++ home-work-18/js/main.js
@@ -0,0 +1,22 @@
+let myNum = 10;
+let myStr = "some string";
+let myBool = true;
+let myArr = [1, 2, 3, 4, 5];
+const myObj = {
+  first: "First Name",
+  last: "Last Name",
+};
+
+console.log(myNum);
+console.log(myStr);
+console.log(myBool);
+console.log(myArr);
+console.log(myObj);
+
+const Num = 10;
+const decimal2 = Num.toFixed(2);
+console.log(decimal2);
+
+let BigInt = 123n;
+myBigInt = BigInt + 1n;
+console.log(myBigInt);
